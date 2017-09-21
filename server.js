499edecc7645d65ef54d37fea7d6d920d02f53c4
smarkov <misticosmisticos@gmail.com>
2017-09-21 18:39:33 +0300

    Rework of structure

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
+'use strict';
 const http = require('http');
 
 const hostname = 'localhost';
-const port = 8080;
+const port = process.env.PORT || 8081
 
 const server = http.createServer((request, response) => {
   response.statusCode = 200;
   response.setHeader('Content-type', 'text/plain');
-  response.end('Hello from misticos.info\n')
+  response.end(`<h1> Hello from misticos.info</h1>`);
 });
 
 server.listen(port, hostname, () => {

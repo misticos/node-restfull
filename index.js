22dbc96b66264b9092953a8c6be8061121968276
smarkov <misticosmisticos@gmail.com>
2017-09-21 16:39:33 +0300

    Initial server is ready

--- index.js
+++ index.js
@@ -3,8 +3,12 @@ const https = require('http');
 const hostname = 'misticos.info';
 const port = 80;
 
-const server = http.createServer((request, response)) => {
+const server = http.createServer((request, response) => {
     response.statusCode = 200;
     response.setHeader('Content-type', 'text/plain');
     response.end('Hello from misticos.info\n')
-}
+});
+
+server.listen(port, hostname, () => {
+    console.log('Server is running at http://${hostname}:${port}');
+})

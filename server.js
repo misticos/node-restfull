9b0b93d815132a352ee23dec338b02f5813947df
smarkov <misticosmisticos@gmail.com>
2017-09-27 16:37:58 +0300

    Server final

--- server.js
+++ server.js
@@ -1,30 +1,97 @@
 'use strict';
-//const http = require('http');
+
 const express = require('express');
 const app = express();
-const bodyparser = require('body-parser'); // req.body
+const bodyParser = require('body-parser');
 const cors = require('cors');
 
-app.use(bodyparser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
+let contacts = require('./data');
+
 app.get('/api/contacts', (request, response) => {
-  response.send('Hello from server.js')
+  if (!contacts) {
+    response.status(404).json({ message: 'No contacts found.' });
+  }
+  response.json(contacts);
 });
 
-// GET, POST, PUT, DELETE, PATCH
+app.get('/api/contacts/:id', (request, response) => {
+
+  let contactId = request.params.id;
+
+  let contact = contacts.filter(contact => {
+    return contact.id == contactId;
+  });
+
+  if (!contact) {
+    response.status(404).json({ message: 'Contact not found' });
+  }
+
+  response.json(contact[0]);
+});
+
+app.post('/api/contacts', (request, response) => {
+
+  let contact = {
+    id: contacts.length + 1,
+    first_name: request.body.first_name,
+    last_name: request.body.last_name,
+    email: request.body.email,
+    website: request.body.website
+  };
+
+  contacts.push(contact);
+
+  response.json(contact);
+
+});
+
+app.put('/api/contacts/:id', (request, response) => {
+
+  let contactId = request.params.id;
+
+  let contact = contacts.filter(contact => {
+    return contact.id == contactId;
+  })[0];
+
+  const index = contacts.indexOf(contact);
+
+  let keys = Object.keys(request.body);
+
+  keys.forEach(key => {
+    contact[key] = request.body[key];
+  });
+
+  contacts[index] = contact;
+
+  // response.json({ message: `User ${contactId} updated.`});
+  response.json(contacts[index]);
+});
+
+app.delete('/api/contacts/:id', (request, response) => {
+  
+  let contactId = request.params.id;
+
+  let contact = contacts.filter(contact => {
+    return contact.id == contactId;
+  })[0];
+
+  const index = contacts.indexOf(contact);
+
+  contacts.splice(index, 1);
+
+  response.json({ message: `User ${contactId} deleted.`});
+
+});
 
 const hostname = 'localhost';
-const port = process.env.PORT || 8081
+const port = 8081;
 
-server.listen(port, hostname, () => {
-  console.log(`Server is running at http://${hostname}:${port}/`);
-});
+const server = app.listen(port, hostname, () => {
 
-/*
-const server = http.createServer((request, response) => {
-  response.statusCode = 200;
-  response.setHeader('Content-type', 'text/plain');
-  response.end(`<h1> Hello from misticos.info</h1>`);
+  console.log(`Server running at http://${hostname}:${port}/`);
+  
 });
-*/
